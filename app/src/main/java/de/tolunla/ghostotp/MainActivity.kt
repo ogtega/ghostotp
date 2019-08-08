a75bb7a13306bf17f8893f30a268c217161fc9f0
Teslim Olunlade <tolunlade@outlook.com>
2019-08-08 09:53:14 -0700

    Setup with placeholder nav_graph and minimal theming

--- app/src/main/java/de/tolunla/ghostotp/MainActivity.kt
+++ app/src/main/java/de/tolunla/ghostotp/MainActivity.kt
@@ -2,11 +2,22 @@ package de.tolunla.ghostotp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import de.tolunla.ghostotp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
+
+        binding.appbar.setupWithNavController(navController)
     }
 }
