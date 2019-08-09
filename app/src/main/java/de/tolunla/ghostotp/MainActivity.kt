ac246d7250755d0835c3078581aa5fa6c6108951
Teslim Olunlade <tolunlade@outlook.com>
2019-08-08 22:03:53 -0700

    Created bottom sheet destinations with better handling of appbar switching

--- app/src/main/java/de/tolunla/ghostotp/MainActivity.kt
+++ app/src/main/java/de/tolunla/ghostotp/MainActivity.kt
@@ -6,25 +6,30 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
+import androidx.navigation.NavController.OnDestinationChangedListener
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import de.tolunla.ghostotp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var appBarConfig: AppBarConfiguration
-    lateinit var binding: ActivityMainBinding
-    lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
-    val onDestinationChanged = NavController.OnDestinationChangedListener { _, destination, _ ->
-        if (destination.id == R.id.otplist) {
+    // Called once the host fragment switches to a new destination
+    private val onDestinationChanged = OnDestinationChangedListener { _, destination, _ ->
+        if (appBarConfig.topLevelDestinations.contains(destination.id)) {
             binding.toolbar.visibility = View.GONE
             binding.appbar.visibility = View.VISIBLE
+            binding.fab.visibility = View.VISIBLE
         } else {
             binding.toolbar.visibility = View.VISIBLE
             binding.appbar.visibility = View.GONE
+            binding.fab.visibility = View.GONE
         }
     }
 
@@ -33,17 +38,33 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navController = findNavController(R.id.nav_host_fragment)
-        appBarConfig = AppBarConfiguration(navController.graph)
+
+        // Used to know when we are at a top level destination
+        appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.otp_list_dest,
+                R.id.create_otp_sheet_dest,
+                R.id.menu_sheet_dest
+            )
+        )
 
         setSupportActionBar(binding.toolbar)
         setupActionBarWithNavController(navController, appBarConfig)
         binding.appbar.setOnMenuItemClickListener(this::onOptionsItemSelected)
 
         navController.addOnDestinationChangedListener(onDestinationChanged)
+
+        binding.fab.setOnClickListener {
+            navController.navigate(R.id.action_otp_list_dest_to_create_otp_sheet_dest)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return item.onNavDestinationSelected(navController) ||
                 super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfig)
     }
 }
