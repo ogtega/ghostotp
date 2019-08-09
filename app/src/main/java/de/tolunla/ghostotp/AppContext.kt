ad0df33647ddf18c5983b740bd383d86d20c1307
Teslim Olunlade <tolunlade@outlook.com>
2019-08-09 15:43:38 -0700

    Updated icon drawable colors and added explicitly delegated night mode to the system

--- app/src/main/java/de/tolunla/ghostotp/AppContext.kt
+++ app/src/main/java/de/tolunla/ghostotp/AppContext.kt
@@ -1,5 +1,15 @@
 package de.tolunla.ghostotp
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
-class AppContext : Application()
+class AppContext : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        )
+    }
+}
