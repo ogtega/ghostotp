3198b3cbfc16fd14eb8e1fdab76ad329c2a4b50d
Teslim Olunlade <tolunlade@outlook.com>
2019-08-14 15:15:15 -0700

    Completed addition of Account

--- app/src/main/java/de/tolunla/ghostotp/view/fragment/NewAccountFragment.kt
+++ app/src/main/java/de/tolunla/ghostotp/view/fragment/NewAccountFragment.kt
@@ -8,8 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import de.tolunla.ghostotp.R
 import de.tolunla.ghostotp.databinding.FragmentNewAccountBinding
+import de.tolunla.ghostotp.db.AppDatabase
+import de.tolunla.ghostotp.db.entity.AccountEntity
+import de.tolunla.ghostotp.db.entity.AccountEntity.Type
 import de.tolunla.ghostotp.showSoftKeyboard
 import org.apache.commons.codec.binary.Base32
 
@@ -18,6 +22,8 @@ class NewAccountFragment : Fragment(), TextWatcher {
   private var prev = 0  // Pointer for the previous secret key character location
   private var current = 1 // Pointer to the current secret key character location
   private val base32Chars = Regex("[A-Za-z2-7=]") // Used to match legal base32 chars
+
+  private lateinit var typeAdapter: ArrayAdapter<String>
 
   private lateinit var binding: FragmentNewAccountBinding
 
@@ -37,21 +43,25 @@ class NewAccountFragment : Fragment(), TextWatcher {
 
       context?.let {
 
-        inputAuthType.setAdapter(
-          ArrayAdapter(
-            it,
-            R.layout.support_simple_spinner_dropdown_item,
-            arrayOf(
-              getString(R.string.label_time_based),
-              getString(R.string.label_counter_based)
-            )
+        typeAdapter = ArrayAdapter(
+          it,
+          R.layout.support_simple_spinner_dropdown_item,
+          arrayOf(
+            getString(R.string.label_time_based),
+            getString(R.string.label_counter_based)
           )
         )
 
+        inputAuthType.setAdapter(typeAdapter)
+
         buttonAdd.setOnClickListener { _ ->
           if (validateSecret(true)) {
-            // TODO: Neatly map user inputs to an AccountEntity
-            // AppDatabase.getInstance(it).accountDao().insertAccount()
+            // TODO: Move this to a background thread
+            AppDatabase.getInstance(it).accountDao().insertAccount(
+              AccountEntity(getAccountName(), getSecretKey(), type = getAuthType())
+            )
+
+            findNavController().navigate(R.id.account_list_dest)
           }
         }
       }
@@ -67,11 +77,14 @@ class NewAccountFragment : Fragment(), TextWatcher {
     context?.let { binding.inputAccountName.showSoftKeyboard(it) }
   }
 
-  private fun getSecretKey(): String {
+  private fun getSecretKey(): String = binding.inputSecretKey.text.toString()
+    .replace(" ", "")
 
-    return binding.inputSecretKey.text.toString()
-      .replace(" ", "")
-      .toUpperCase()
+  private fun getAccountName(): String = binding.inputAccountName.text.toString()
+
+  private fun getAuthType(): Type {
+    val pos = typeAdapter.getPosition(binding.inputAuthType.text.toString())
+    return (if (pos == 0) Type.TOTP else Type.HOTP)
   }
 
   private fun validateSecret(toSubmit: Boolean = false): Boolean {
@@ -89,8 +102,6 @@ class NewAccountFragment : Fragment(), TextWatcher {
 
       binding.layoutKeyInput.error = null
     } catch (e: IllegalArgumentException) {
-
-      println(e.message)
 
       binding.layoutKeyInput.error =
         (if (toSubmit) getString(R.string.message_key_invalid_chars) else null)
