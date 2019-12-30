2bf1e2b4efca0d8e5342320f9f3d8150e5bf8179
Teslim Olunlade <tolunlade@outlook.com>
2019-12-30 09:47:31 -0500

    Added WebView from compiled Steam Vue Project

---
+++ app/src/main/java/de/tolunla/ghostotp/view/fragment/NewPlatformFragment.kt
@@ -0,0 +1,43 @@
+package de.tolunla.ghostotp.view.fragment
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.tolunla.ghostotp.R
+import de.tolunla.ghostotp.databinding.FragmentNewPlatformBinding
+
+class NewPlatformFragment : Fragment() {
+
+  private var platform = -1
+  private lateinit var binding: FragmentNewPlatformBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    arguments?.let {
+      platform = it.getInt("dest")
+    }
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?): View? {
+    binding = FragmentNewPlatformBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  @SuppressLint("SetJavaScriptEnabled")
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.webView.settings.javaScriptEnabled = true
+
+    binding.webView.loadUrl(when (platform) {
+      R.id.new_steam_dest -> "file:///android_asset/steamauth/index.html"
+      else -> ""
+    })
+  }
+}
--- app/src/main/java/de/tolunla/ghostotp/view/fragment/sheet/NewAccountSheet.kt
+++ app/src/main/java/de/tolunla/ghostotp/view/fragment/sheet/NewAccountSheet.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.onNavDestinationSelected
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import de.tolunla.ghostotp.R
 import de.tolunla.ghostotp.databinding.SheetNewAccountBinding
 
 class NewAccountSheet : BottomSheetDialogFragment() {
@@ -30,6 +33,15 @@ class NewAccountSheet : BottomSheetDialogFragment() {
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    val platforms = setOf(R.id.new_steam_dest)
+
+    if (platforms.contains(item.itemId)) {
+      val bundle = bundleOf("dest" to item.itemId)
+      activity?.findViewById<View>(R.id.nav_host_fragment)?.findNavController()
+        ?.navigate(R.id.new_platform_dest, bundle)
+      return super.onOptionsItemSelected(item)
+    }
+
     return item.onNavDestinationSelected(findNavController()) ||
         super.onOptionsItemSelected(item)
   }
