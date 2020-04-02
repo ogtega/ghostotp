d274bc42cbcebdcfd99d3f870220567293ed8d9f
Teslim Olunlade <tolunlade@outlook.com>
2020-04-02 05:45:05 -0400

    Further Gradle COnfiguration

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,8 +2,8 @@ object Versions {
     val kotlin = "1.3.61"
     val lifecycle = "2.2.0"
     val navigation = "2.2.1"
-    val room = "2.2.4"
-    val work = "2.3.3"
+    val room = "2.2.5"
+    val work = "2.3.4"
     val build_tools = "29.0.3"
 }
 
